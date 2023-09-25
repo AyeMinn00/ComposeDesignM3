600b89e9c1c7d9dd3e970b2fda3b8ded9f7165f5
AyeMin00 <ayeminnoo.dev@gmail.com>
2023-09-26 00:29:46 +0700

    style: use dynamic color

--- app/src/main/java/com/example/reply/ui/theme/Theme.kt
+++ app/src/main/java/com/example/reply/ui/theme/Theme.kt
@@ -16,11 +16,15 @@
 
 package com.example.reply.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 // Material 3 color schemes
 private val DarkColorScheme = darkColorScheme(
@@ -93,12 +97,18 @@ fun ReplyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val replyColorScheme = when {
+    val context = LocalContext.current
+    val colors = when {
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (darkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     MaterialTheme(
-        colorScheme = replyColorScheme,
+        colorScheme = colors,
         content = content
     )
 }
